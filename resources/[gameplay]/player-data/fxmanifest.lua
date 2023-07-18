c4c8bed3da5d68b779b5c35f53f7291189652d0a
Brian Tambara <btambara@gmail.com>
2023-07-17 21:01:01 -0700

    update player-data to tofu-core

--- resources/[gameplay]/player-data/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'Brian Tambara <btambara@gmail.com>'
-description 'A basic resource for storing player identifiers.'
-version '1.0.0'
-
-client_script 'dist/client/*.client.js'
-server_script 'dist/server/*.server.js'
